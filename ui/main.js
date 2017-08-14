0bda34cd2bf7404a47d6ee7f1c0c3cab536efef8
Akash Ravi <akashkravi@gmail.com>
2017-08-14 12:35:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element=document.getElementById('main-text');
 element.innerHTML='new text updated';
 
 var img=document.getElementById('madi');
-img.onclick= function(){
-    img.style.marginleft='100px';
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft +'px';
 }
+img.onclick= function(){
+    var interval=setInterval(moveRight, 100);
+    
+};
